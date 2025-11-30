5d8800e56b5c238e3fab395fc23c5c2faf27d355
adminsyspro <51157323+adminsyspro@users.noreply.github.com>
2025-11-30 09:31:34 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="light">
       <Head>
         {/* <!-- Cloudflare Pages Custom Error Pages --> */}
         <meta name="description" content={siteConfig.description} />
